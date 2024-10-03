bfd797f5b534b7c79c5f761c5a2eaf69a56af80d
kenan <kenanomerbegovic@gmail.com>
2024-10-03 16:16:27 +0200

    update

--- Form1.cs
+++ Form1.cs
@@ -22,17 +22,27 @@ namespace picture_viewer
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void clearButton_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void closeButton_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void bgButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
