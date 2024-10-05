b111892e1957dd2a2c050c00108b4fe241f67a58
kenan <kenanomerbegovic@gmail.com>
2024-10-05 17:33:12 +0200

    final

--- Form1.cs
+++ Form1.cs
@@ -24,27 +24,37 @@ namespace picture_viewer
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void bgButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            
+                
+            
         }
     }
 }
